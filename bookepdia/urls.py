8c855f7679e208175998ef0fa2f4eadc593d807a
akanksha007 <akankshaagarwal007@gmail.com>
2015-08-19 11:12:48 +0530

    partial commit

--- bookepdia/urls.py
+++ bookepdia/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 import os
 
 urlpatterns = patterns('',
@@ -11,7 +12,9 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^site_media/(.*)$', 'django.views.static.serve', {'document_root' : settings.MEDIA_ROOT}),
     
-)
+    
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 if settings.DEBUG:
     # static files (images, css, javascript, etc.)
---
+++ readme.md~
@@ -0,0 +1,6 @@
+Webapp using django framework.
+Webapp should have multiple images displayed on home page, of multiple items. 
+Hovering on image should show item's name. 
+Clicking on each image should open a popup with brief description of the content related to item. 
+Read more option should go to a new page where the entire page is dedicated to details of that item. 
+You can think of what elements can be added in the dedicated page, eventually.
